e3e80afeea17ac6f81376320cc5d19d05c541551
levanhaoslvp <levanhaoslvp@gmail.com>
2021-05-17 11:53:19 +0700

    refs #1 login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root    'static_p_age#home'
-  get     '/home',      to: 'static_p_age#home'
-  get     '/help',      to: 'static_p_age#help', as: 'helf'
-  get     '/hello',     to: 'static_p_age#hello'
-  get     '/about',     to: 'static_p_age#about'
-  get     '/contact',   to: 'static_p_age#contact'
-  get     '/signup',    to: 'users#new'
-  get     '/login',     to: 'session#new'
-  post    '/login',     to: 'session#create'
-  delete  '/login',     to: 'session#destroy'
-  get 'sessions/new'
-  resources :users
-  resources :account_activations, only: [:edit]
+  get     '/home', to: 'static_p_age#home'
   resources :microposts, only: %i[create destroy]
 end
