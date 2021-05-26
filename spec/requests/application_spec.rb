b9cc663769480bb68e671cd7847364ce65334baf
levanhaoslvp <levanhaoslvp@gmail.com>
2021-05-26 14:47:38 +0700

    add database.yml

---
+++ spec/requests/application_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Applications', type: :request do
+  describe 'GET #index' do
+    it 'returns a 302 OK status' do
+      get '/home'
+      expect(response).to have_http_status(302)
+    end
+  end
+end
