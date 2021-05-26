35d210b74086151755b23cb98f0bf1173e279a0b
levanhaoslvp <levanhaoslvp@gmail.com>
2021-05-26 14:06:22 +0700

    add ci config : check rubocop, test

--- app/controllers/static_p_age_controller.rb
+++ app/controllers/static_p_age_controller.rb
@@ -3,13 +3,7 @@
 # app/controllers/static_p_age.rb
 
 class StaticPAgeController < ApplicationController
-  def home
-    if logged_in?
-      @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page:
-      params[:page])
-    end
-  end
+  def home; end
 
   def help; end
 
