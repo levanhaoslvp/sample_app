522042417da4d1b2c21092f8412e010e763011ba
levanhaoslvp <levanhaoslvp@gmail.com>
2021-05-26 18:34:08 +0700

    fix rubocop.yml

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,30 @@
 # frozen_string_literal: true
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  include OmniauthHelper
-  def facebook
-    generic_callback('facebook')
-  end
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    include OmniauthHelper
+    def facebook
+      generic_callback("facebook")
+    end
 
-  def google_oauth2
-    generic_callback('google_oauth2')
-  end
+    def google_oauth2
+      generic_callback("google_oauth2")
+    end
 
-  private
+    private
 
-  def generic_callback(provider)
-    auth = request.env['omniauth.auth']
-    @identity = User.from_omniauth(auth)
-    check_provider(auth)
-    @user = @identity || current_user
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      session['provider'] = auth.provider
-      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+    def generic_callback provider
+      auth = request.env["omniauth.auth"]
+      @identity = User.from_omniauth(auth)
+      check_provider(auth)
+      @user = @identity || current_user
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        session["provider"] = auth.provider
+      else
+        session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
   end
 end
