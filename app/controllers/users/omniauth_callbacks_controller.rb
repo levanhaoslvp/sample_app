b2855cd66e0aac70acb28aa512099751d5746c69
levanhaoslvp <56867115+levanhaoslvp@users.noreply.github.com>
2021-05-31 09:13:33 +0700

    refs #2 config Circle CI/CD (#14)
    * config new cicd build-rubocop-test
    
    * config new cicd build-rubocop-test- fix gemfile
    
    * config new cicd build-rubocop-test- fix gemfile
    
    * config new cicd build-rubocop-test- fix syntax
    
    * add database.yml
    
    * config deploy heroku
    
    * config deploy heroku fix syntax
    
    * fix ci/ci deploy

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,26 +3,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include OmniauthHelper
   def facebook
-    generic_callback('facebook')
+    generic_callback("facebook")
   end
 
   def google_oauth2
-    generic_callback('google_oauth2')
+    generic_callback("google_oauth2")
   end
 
   private
 
-  def generic_callback(provider)
-    auth = request.env['omniauth.auth']
+  def generic_callback provider
+    auth = request.env["omniauth.auth"]
     @identity = User.from_omniauth(auth)
     check_provider(auth)
     @user = @identity || current_user
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      session['provider'] = auth.provider
-      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
+      session["provider"] = auth.provider
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
