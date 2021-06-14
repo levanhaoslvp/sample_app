2b92a5c4034a4d4585b9f16a81b95d3abce6d5c5
levanhaoslvp <levanhaoslvp@gmail.com>
2021-06-14 11:57:36 +0700

    fix test

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Applications', type: :request do
   describe 'GET #index' do
     it 'returns a 302 OK status' do
       get root_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
