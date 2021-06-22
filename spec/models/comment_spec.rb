a82294fdeceb6ea489e3e0fce38a09aab36b0ad4
levanhaoslvp <levanhaoslvp@gmail.com>
2021-06-22 11:35:18 +0700

    rut gon rspec model (1)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:current_user){
-    create(:user)
-  }
+  let(:current_user){ create(:user)}
   
   describe "Associations" do
     it { should belong_to(:user) }
@@ -17,8 +15,8 @@ RSpec.describe Comment, type: :model do
   end
 
   it "has a content" do
-    comment = build(:comment_no_content,:user => current_user)
-    comment.post = create(:post, :user => current_user)
+    comment = build(:comment_no_content,user: current_user)
+    comment.post = create(:post, user: current_user)
     expect(comment).to_not be_valid
 
     comment.content = "my comment"
@@ -26,16 +24,16 @@ RSpec.describe Comment, type: :model do
   end
 
   it "has a post" do
-    comment = build(:comment, :user => current_user)
+    comment = build(:comment, user: current_user)
     expect(comment).to_not be_valid
 
-    comment.post = create(:post, :user => current_user)
+    comment.post = create(:post, user: current_user)
     expect(comment).to be_valid
   end
 
   it "has a user" do
     comment = build(:comment)
-    comment.post = create(:post, :user => current_user)
+    comment.post = create(:post, user: current_user)
     expect(comment).to_not be_valid
 
     comment.user = current_user
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   
-  let(:current_user){
-    create(:user)
-  }
+  let(:current_user){ create(:user)}
   
   describe "Associations" do
     it { should belong_to(:user) }
@@ -25,7 +23,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has a title' do
-    post = build(:post_no_title, :user => current_user)
+    post = build(:post_no_title, user: current_user)
     expect(post).to_not be_valid
 
     post.title = 'Has a title'
@@ -33,7 +31,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has a content' do
-    post = build(:post_no_content, :user => current_user)
+    post = build(:post_no_content, user: current_user)
     expect(post).to_not be_valid
 
     post.content = 'Has a title'
@@ -41,7 +39,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has a title at least 2 characters long' do
-    post = build(:post, :title => '1', :user => current_user)
+    post = build(:post, title: '1', user: current_user)
     expect(post).to_not be_valid
 
     post.title = '12'
@@ -49,7 +47,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has a content between 5 and 100 characters' do
-    post = build(:post, :content => '1234', :user => current_user)
+    post = build(:post, content: '1234', user: current_user)
     expect(post).to_not be_valid
 
     post.content = '12345'
@@ -65,7 +63,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has a image' do
-    post = build(:post_no_image, :user => current_user)
+    post = build(:post_no_image, user: current_user)
     expect(post).to be_valid
 
     post.image = 'https://vcdn-thethao.vnecdn.net/2021/06/20/1-2426-1624149210.jpg'
