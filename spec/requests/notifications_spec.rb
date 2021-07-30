9efbc27aa80cef88fe5132412c44b8c3cd589a6b
levanhaoslvp <levanhaoslvp@gmail.com>
2021-07-30 08:22:28 +0700

    fix test ,rails_helper, fix current_user

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
-  let(:current_user){ create(:user) }
+  let(:user){ create(:user) }
   let(:other_user){ create(:user, email: "test99@gmail.com") }
-  let(:test_post){ create(:post, user: current_user)}
+  let(:test_post){ create(:post, user: user)}
 
-  before { sign_in current_user}
+  before { sign_in user}
 
   describe "no loggin" do
-    before {sign_out current_user}
+    before {sign_out user}
 
     it "redirect to sign-in when POST /create" do
-      post notifications_url(id: current_user.id)
+      post notifications_url(id: user.id)
       expect(response).to redirect_to(new_user_session_url)
     end
   end
 
   describe " logged " do
     it "renders a successful response when POST /create" do
-      post notifications_url(), params: {id: current_user.id}, xhr: true
-      expect(response).to render_template("notifications/create")
+      post notifications_url(), params: {id: user.id}, xhr: true
+      expect(response).to render_template('notifications/create')
     end
 
     it "set viewed for Notification" do
-      noti_new = create(:notification, user: other_user, recipient: current_user, notifiable: test_post, viewed: false)
-      post notifications_url(), params: {id: current_user.id}, xhr: true
+      noti_new = create(:notification, user: other_user, recipient: user, notifiable: test_post, viewed: false)
+      post notifications_url(), params: {id: user.id}, xhr: true
       expect(noti_new.reload.viewed).to eq(true)
     end
   end
