553947ad710fcad37e593b8ed90c0e74e3271b73
levanhaoslvp <levanhaoslvp@gmail.com>
2021-08-10 16:42:38 +0700

    fix config circleci

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 # Model User
 class User < ApplicationRecord
-  #rolify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable,
