d2e1d16ff9d862654cda5d166d56540c0f81e6ff
levanhaoslvp <levanhaoslvp@gmail.com>
2021-08-11 13:04:22 +0700

    fix rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 # app/controllers/UsersController
 class UsersController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
